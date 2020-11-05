3ecb87b30e41f5f8ec6dc1da5aeb29882185001c
Danny <tekhexrax@yahoo.com>
2020-11-05 16:40:04 -0500

    Added require to images

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Project = (props) => {
     const { name, image, alt, url, text, github } = props
+    console.log(image)
+ 
         return (
         <React.Fragment>            
                 <div className="col-sm-12 col-md-6 col-lg-4 card-block">
                     <div className="card">
                         <a href={url} target="_blank" rel="noreferrer">
-                            <img src={image} className="card-img-top projimg" alt={alt} /></a>
+                            <img src={require(`../images/${image}`)} className="card-img-top projimg" alt={alt} /></a>
                         <div className="card-body">
                             <h5 className="card-title">{name}</h5>
                             <p className="card-text">{text}</p>
