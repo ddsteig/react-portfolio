a59edbba8e9ff1dd1c645d09dbb7eeb3e168ad4f
Danny <tekhexrax@yahoo.com>
2020-11-05 14:37:40 -0500

    Updated package.json

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div>
         <Nav/>
         <Wrapper>
-          <Route exact path="/" component={About} />
+          <Route exact path="/" component={About}/>
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
